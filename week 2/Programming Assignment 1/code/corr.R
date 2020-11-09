22eb7b4ffa8382d11df117b64c15f89b5e2f0b68
emotet <esali@gmx.de>
2020-11-09 19:53:11 +0100

    completed Programming Assignment 1

--- week 2/Programming Assignment 1/code/corr.R
+++ week 2/Programming Assignment 1/code/corr.R
@@ -1,14 +1,22 @@
-corr <- function(directory, thresold = 0){
-        filesList <- list.files(path = directory, pattern = ".csv", full.names = TRUE)
+
+corr <- function(directory, threshold = 0) {
+        id = 1:332
+        filename <- list.files(directory, full.names = TRUE)
         
-        num <- numeric()
+        result <-vector(mode="numeric", length=0)
         
-        for(i in filesList){
-                data <- read.csv(filesList[i])
-                num <- complete.cases(data)
-                cor(sulfate, nitrate, use = "all.obs" > thresold, method = c())
-                
+        for(i in seq(filename)) {
+                airquality <- read.csv(filename[i])
+                good <- complete.cases(airquality)
+                airquality <- airquality[good, ]
+                if (nrow(airquality) > threshold) {
+                        # We need [[]] around pollutant instead of [] since airquality["sulfate"]
+                        # is a data.frame but we need a vector here. Hence, [[]]. Please note thatusing either
+                        #[[]] or [] gives the same results as the test cases
+                        correlation <- cor(airquality[["sulfate"]], airquality[["nitrate"]])
+                        result <- append(result, correlation)
+                        #print(correlation)
+                }
         }
-        
-        
+        result
 }
